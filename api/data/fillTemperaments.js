0937bc18a3785d32ce5eeac6421b87abd8cf828d
Carlos7979 <carloscorredorwg@gmail.com>
2021-08-09 00:21:43 -0400

    add finished fillTemperaments functionality

--- api/data/fillTemperaments.js
+++ api/data/fillTemperaments.js
@@ -1,8 +1,27 @@
 const { axios } = require('common');
+const { Temperament } = require('../src/db');
 
 async function fillTemperaments() {
-    const dogs = await axios.get('https://api.thedogapi.com/v1/breeds');
-    console.log(dogs);
+    try {
+        const response = await axios.get('https://api.thedogapi.com/v1/breeds');
+        const dogs = response.data;
+        for (const dog of dogs) {
+            if (dog.temperament) {
+                const temperaments = dog.temperament.split(', ');
+                if (temperaments.length > 0) {
+                    for (const temperament of temperaments) {
+                        await Temperament.findOrCreate({
+                            where: {
+                                name: temperament
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        return console.log('Error: ', error);
+    }
 }
 
 module.exports = fillTemperaments;
