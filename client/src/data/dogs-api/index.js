2d8c83c804102cf0aff8866ae7cb9500e4c1e7cc
Carlos7979 <carloscorredorwg@gmail.com>
2021-08-15 14:49:32 -0400

    add call function into data layer

---
+++ client/src/data/dogs-api/index.js
@@ -0,0 +1,27 @@
+// import { axios } from 'common';
+const { axios } = require('common');
+
+function call(url, options = {}) {
+    const { method = 'GET', body } = options;
+    // TODO validate
+    let axiosCall = axios.get;
+    if (method === 'POST') axiosCall = axios.post;
+    return (async () => {
+        try {
+            const response = await axiosCall(url, body);
+            return response.data;
+        } catch (error) {
+            if (error.code === 'ECONNREFUSED') return console.log('Cannot connect');
+            if (error.response && error.response.status) {
+                return {
+                    message: error.response.statusText,
+                    status: error.response.status
+                }
+            }
+            return console.log('Error: ', error);
+        }
+    })();
+}
+
+module.exports = call;
+// export default call;
---
+++ client/src/logic/actions.js
@@ -0,0 +1,8 @@
+const call = require('../data/dogs-api');
+
+
+
+// (async () => {
+//     const result = await call('http://localhost:3001/api-dogs/temperaments');
+//     console.log(result);
+// })();
